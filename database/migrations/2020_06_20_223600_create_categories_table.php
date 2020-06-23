724e7c67bbacb28d03bea49b54bdb80f3e18a620
Alvaro Mazuera U <am@alvaromazuera.com>
2020-06-23 12:03:21 -0500

    Added softdeletes for Category

--- database/migrations/2020_06_20_223600_create_categories_table.php
+++ database/migrations/2020_06_20_223600_create_categories_table.php
@@ -18,6 +18,7 @@ class CreateCategoriesTable extends Migration
             $table->string("name", 100);
             $table->string("code", 100);
             $table->text("description");
+            $table->softDeletes();
         });
     }
 
