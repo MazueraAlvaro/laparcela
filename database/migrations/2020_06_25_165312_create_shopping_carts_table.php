bdb3c5332060d213bb82c9112c3312320c13d411
Alvaro Mazuera U <am@alvaromazuera.com>
2020-06-25 17:36:14 -0500

    Added Shopping Cart model

---
+++ database/migrations/2020_06_25_165312_create_shopping_carts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShoppingCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shopping_carts', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->foreignId("session_id")->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('shopping_carts', function (Blueprint $table) {
+            $table->dropForeign(["session_id"]);
+        });
+        Schema::dropIfExists('shopping_carts');
+    }
+}
