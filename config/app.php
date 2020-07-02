7854ffa284338a53d3a75509f870df7d3e96b434
Alvaro Mazuera U <am@alvaromazuera.com>
2020-07-02 17:55:49 -0500

    Added helper package

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        browner12\helpers\HelperServiceProvider::class,
 
     ],
 
